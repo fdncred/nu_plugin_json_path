381fca9d93d6594128479cdde9fef678aa2d7ff4
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2023-02-20 14:40:15 -0600

    handle input of record or string

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use nu_protocol::{
     ast::PathMember, Category, PluginExample, PluginSignature, ShellError, Spanned, SyntaxShape,
     Value,
 };
-use serde_json::{json, Value as SerdeJsonValue};
+use serde_json::Value as SerdeJsonValue;
 use serde_json_path::JsonPathExt;
 
 // json path examples
@@ -32,8 +32,8 @@ impl Plugin for JsonPath {
             .required("query", SyntaxShape::String, "json path query")
             .category(Category::Experimental)
             .plugin_examples(vec![PluginExample {
-                description: "This is the example descripion".into(),
-                example: "some pipeline involving json_path".into(),
+                description: "List the authors of all books in the store".into(),
+                example: "open -r test.json | json path '$.store.book[*].author'".into(),
                 result: None,
             }])]
     }
@@ -46,16 +46,28 @@ impl Plugin for JsonPath {
     ) -> Result<Value, LabeledError> {
         assert_eq!(name, "json path");
         let param: Option<Spanned<String>> = call.opt(0)?;
-
         let span = call.head;
-        // let value = input.into_value(span);
-        let json_value = value_to_json_value(&input)?;
-        let serde_json: SerdeJsonValue = serde_json::from_str(&input.as_string()?).unwrap();
 
+        eprintln!("input_type: {:?}", input.get_type());
         let json_path_results = match input {
             Value::String { val, span } => {
-                // crate::json_path::json_path_do_something(param, val, *span)?
-                // json_value.json_path(&param.unwrap().item).unwrap().all()
+                let serde_json: SerdeJsonValue = serde_json::from_str(&input.as_string()?).unwrap();
+
+                serde_json
+                    .json_path(&param.unwrap().item)
+                    .unwrap()
+                    .all()
+                    .into_iter()
+                    .map(|v| Value::String {
+                        val: v.to_string(),
+                        span: *span,
+                    })
+                    .collect()
+            }
+            Value::Record { cols, vals, span } => {
+                let json_value = value_to_json_value(&input)?;
+                let raw = serde_json::to_string(&json_value).unwrap();
+                let serde_json: SerdeJsonValue = serde_json::from_str(&raw).unwrap();
                 serde_json
                     .json_path(&param.unwrap().item)
                     .unwrap()
@@ -68,6 +80,7 @@ impl Plugin for JsonPath {
                     .collect()
             }
             v => {
+                eprintln!("here");
                 return Err(LabeledError {
                     label: "Expected something from pipeline".into(),
                     msg: format!("requires some input, got {}", v.get_type()),
@@ -76,45 +89,76 @@ impl Plugin for JsonPath {
             }
         };
 
+        // let json_value = value_to_json_value(&input)?;
+        // let raw = serde_json::to_string(&json_value).unwrap();
+        // eprintln!("raw: {}", raw);
+        // eprintln!("before serde_json::from_str");
+        // let serde_json: SerdeJsonValue = serde_json::from_str(&input.as_string()?).unwrap();
+        // eprintln!("after serde_json::from_str");
+        // let json_path_results = match input {
+        //     Value::String { val, span } => {
+        //         // crate::json_path::json_path_do_something(param, val, *span)?
+        //         // json_value.json_path(&param.unwrap().item).unwrap().all()
+        //         serde_json
+        //             .json_path(&param.unwrap().item)
+        //             .unwrap()
+        //             .all()
+        //             .into_iter()
+        //             .map(|v| Value::String {
+        //                 val: v.to_string(),
+        //                 span: *span,
+        //             })
+        //             .collect()
+        //     }
+        //     v => {
+        //         eprintln!("here");
+        //         return Err(LabeledError {
+        //             label: "Expected something from pipeline".into(),
+        //             msg: format!("requires some input, got {}", v.get_type()),
+        //             span: Some(call.head),
+        //         });
+        //     }
+        // };
+
         let ret_list = Value::List {
             vals: json_path_results,
             span,
         };
 
-        let sample_json = spec_example_json();
-        let example1 = sample_json
-            .json_path("$.store.book[*].author")
-            .unwrap()
-            .all();
-        eprintln!("example1: {:?}", example1);
-        let example2 = sample_json.json_path("$..author").unwrap().all();
-        eprintln!("example2: {:?}", example2);
-        let example3 = sample_json.json_path("$.store.*").unwrap().all();
-        eprintln!("example3: {:?}", example3);
-        let example4 = sample_json.json_path("$.store..price").unwrap().all();
-        eprintln!("example4: {:?}", example4);
-        let example5 = sample_json.json_path("$..book[2]").unwrap().all();
-        eprintln!("example5: {:?}", example5);
-        let example6 = sample_json.json_path("$..book[-1]").unwrap().all();
-        eprintln!("example6: {:?}", example6);
-        let example7 = sample_json.json_path("$..book[0,1]").unwrap().all();
-        eprintln!("example7: {:?}", example7);
-        let example8 = sample_json.json_path("$..book[:2]").unwrap().all();
-        eprintln!("example8: {:?}", example8);
-        let example9 = sample_json.json_path("$..book[?(@.isbn)]").unwrap().all();
-        eprintln!("example9: {:?}", example9);
-        let example10 = sample_json
-            .json_path("$..book[?(@.price<10)]")
-            .unwrap()
-            .all();
-        eprintln!("example10: {:?}", example10);
-        let example11 = sample_json.json_path("$..*").unwrap().all();
-        eprintln!("example11: {:?}", example11);
-        let example12 = sample_json
-            .json_path("$..book[?(@.price<30 && @.price>10)]")
-            .unwrap()
-            .all();
-        eprintln!("example12: {:?}", example12);
+        // let sample_json = spec_example_json();
+        // let example1 = sample_json
+        //     .json_path("$.store.book[*].author")
+        //     .unwrap()
+        //     .all();
+        // eprintln!("example1: {:?}", example1);
+        // let example2 = sample_json.json_path("$..author").unwrap().all();
+        // eprintln!("example2: {:?}", example2);
+        // let example3 = sample_json.json_path("$.store.*").unwrap().all();
+        // eprintln!("example3: {:?}", example3);
+        // let example4 = sample_json.json_path("$.store..price").unwrap().all();
+        // eprintln!("example4: {:?}", example4);
+        // let example5 = sample_json.json_path("$..book[2]").unwrap().all();
+        // eprintln!("example5: {:?}", example5);
+        // let example6 = sample_json.json_path("$..book[-1]").unwrap().all();
+        // eprintln!("example6: {:?}", example6);
+        // let example7 = sample_json.json_path("$..book[0,1]").unwrap().all();
+        // eprintln!("example7: {:?}", example7);
+        // let example8 = sample_json.json_path("$..book[:2]").unwrap().all();
+        // eprintln!("example8: {:?}", example8);
+        // let example9 = sample_json.json_path("$..book[?(@.isbn)]").unwrap().all();
+        // eprintln!("example9: {:?}", example9);
+        // let example10 = sample_json
+        //     .json_path("$..book[?(@.price<10)]")
+        //     .unwrap()
+        //     .all();
+        // eprintln!("example10: {:?}", example10);
+        // let example11 = sample_json.json_path("$..*").unwrap().all();
+        // eprintln!("example11: {:?}", example11);
+        // let example12 = sample_json
+        //     .json_path("$..book[?(@.price<30 && @.price>10)]")
+        //     .unwrap()
+        //     .all();
+        // eprintln!("example12: {:?}", example12);
 
         Ok(ret_list)
     }
@@ -175,138 +219,138 @@ fn main() {
     serve_plugin(&mut JsonPath::new(), MsgPackSerializer);
 }
 
-fn spec_example_json() -> SerdeJsonValue {
-    json!({
-        "store": {
-            "book": [
-                {
-                    "category": "reference",
-                    "author": "Nigel Rees",
-                    "title": "Sayings of the Century",
-                    "price": 8.95
-                },
-                {
-                    "category": "fiction",
-                    "author": "Evelyn Waugh",
-                    "title": "Sword of Honour",
-                    "price": 12.99
-                },
-                {
-                    "category": "fiction",
-                    "author": "Herman Melville",
-                    "title": "Moby Dick",
-                    "isbn": "0-553-21311-3",
-                    "price": 8.99
-                },
-                {
-                    "category": "fiction",
-                    "author": "J. R. R. Tolkien",
-                    "title": "The Lord of the Rings",
-                    "isbn": "0-395-19395-8",
-                    "price": 22.99
-                }
-            ],
-            "bicycle": {
-                "color": "red",
-                "price": 399
-            }
-        }
-    })
-}
+// fn spec_example_json() -> SerdeJsonValue {
+//     json!({
+//         "store": {
+//             "book": [
+//                 {
+//                     "category": "reference",
+//                     "author": "Nigel Rees",
+//                     "title": "Sayings of the Century",
+//                     "price": 8.95
+//                 },
+//                 {
+//                     "category": "fiction",
+//                     "author": "Evelyn Waugh",
+//                     "title": "Sword of Honour",
+//                     "price": 12.99
+//                 },
+//                 {
+//                     "category": "fiction",
+//                     "author": "Herman Melville",
+//                     "title": "Moby Dick",
+//                     "isbn": "0-553-21311-3",
+//                     "price": 8.99
+//                 },
+//                 {
+//                     "category": "fiction",
+//                     "author": "J. R. R. Tolkien",
+//                     "title": "The Lord of the Rings",
+//                     "isbn": "0-395-19395-8",
+//                     "price": 22.99
+//                 }
+//             ],
+//             "bicycle": {
+//                 "color": "red",
+//                 "price": 399
+//             }
+//         }
+//     })
+// }
 
-#[test]
-fn spec_example_1() {
-    let value = spec_example_json();
-    let nodes = value.json_path("$.store.book[*].author").unwrap().all();
-    assert_eq!(
-        nodes,
-        vec![
-            "Nigel Rees",
-            "Evelyn Waugh",
-            "Herman Melville",
-            "J. R. R. Tolkien"
-        ]
-    );
-}
+// #[test]
+// fn spec_example_1() {
+//     let value = spec_example_json();
+//     let nodes = value.json_path("$.store.book[*].author").unwrap().all();
+//     assert_eq!(
+//         nodes,
+//         vec![
+//             "Nigel Rees",
+//             "Evelyn Waugh",
+//             "Herman Melville",
+//             "J. R. R. Tolkien"
+//         ]
+//     );
+// }
 
-#[test]
-fn spec_example_2() {
-    let value = spec_example_json();
-    let nodes = value.json_path("$..author").unwrap().all();
-    assert_eq!(
-        nodes,
-        vec![
-            "Nigel Rees",
-            "Evelyn Waugh",
-            "Herman Melville",
-            "J. R. R. Tolkien"
-        ]
-    );
-}
+// #[test]
+// fn spec_example_2() {
+//     let value = spec_example_json();
+//     let nodes = value.json_path("$..author").unwrap().all();
+//     assert_eq!(
+//         nodes,
+//         vec![
+//             "Nigel Rees",
+//             "Evelyn Waugh",
+//             "Herman Melville",
+//             "J. R. R. Tolkien"
+//         ]
+//     );
+// }
 
-#[test]
-fn spec_example_3() {
-    let value = spec_example_json();
-    let nodes = value.json_path("$.store.*").unwrap().all();
-    assert_eq!(nodes.len(), 2);
-    assert!(nodes
-        .iter()
-        .any(|&node| node == value.pointer("/store/book").unwrap()));
-}
+// #[test]
+// fn spec_example_3() {
+//     let value = spec_example_json();
+//     let nodes = value.json_path("$.store.*").unwrap().all();
+//     assert_eq!(nodes.len(), 2);
+//     assert!(nodes
+//         .iter()
+//         .any(|&node| node == value.pointer("/store/book").unwrap()));
+// }
 
-#[test]
-fn spec_example_4() {
-    let value = spec_example_json();
-    let nodes = value.json_path("$.store..price").unwrap().all();
-    assert_eq!(nodes, vec![399., 8.95, 12.99, 8.99, 22.99]);
-}
+// #[test]
+// fn spec_example_4() {
+//     let value = spec_example_json();
+//     let nodes = value.json_path("$.store..price").unwrap().all();
+//     assert_eq!(nodes, vec![399., 8.95, 12.99, 8.99, 22.99]);
+// }
 
-#[test]
-fn spec_example_5() {
-    let value = spec_example_json();
-    let q = value.json_path("$..book[2]").unwrap();
-    let node = q.one().unwrap();
-    assert_eq!(node, value.pointer("/store/book/2").unwrap());
-}
+// #[test]
+// fn spec_example_5() {
+//     let value = spec_example_json();
+//     let q = value.json_path("$..book[2]").unwrap();
+//     let node = q.one().unwrap();
+//     assert_eq!(node, value.pointer("/store/book/2").unwrap());
+// }
 
-#[test]
-fn spec_example_6() {
-    let value = spec_example_json();
-    let q = value.json_path("$..book[-1]").unwrap();
-    let node = q.one().unwrap();
-    assert_eq!(node, value.pointer("/store/book/3").unwrap());
-}
+// #[test]
+// fn spec_example_6() {
+//     let value = spec_example_json();
+//     let q = value.json_path("$..book[-1]").unwrap();
+//     let node = q.one().unwrap();
+//     assert_eq!(node, value.pointer("/store/book/3").unwrap());
+// }
 
-#[test]
-fn spec_example_7() {
-    let value = spec_example_json();
-    {
-        let q = value.json_path("$..book[0,1]").unwrap();
-        assert_eq!(q.len(), 2);
-    }
-    {
-        let q = value.json_path("$..book[:2]").unwrap();
-        assert_eq!(q.len(), 2);
-    }
-}
+// #[test]
+// fn spec_example_7() {
+//     let value = spec_example_json();
+//     {
+//         let q = value.json_path("$..book[0,1]").unwrap();
+//         assert_eq!(q.len(), 2);
+//     }
+//     {
+//         let q = value.json_path("$..book[:2]").unwrap();
+//         assert_eq!(q.len(), 2);
+//     }
+// }
 
-#[test]
-fn spec_example_8() {
-    let value = spec_example_json();
-    let q = value.json_path("$..book[?(@.isbn)]").unwrap();
-    assert_eq!(q.len(), 2);
-}
+// #[test]
+// fn spec_example_8() {
+//     let value = spec_example_json();
+//     let q = value.json_path("$..book[?(@.isbn)]").unwrap();
+//     assert_eq!(q.len(), 2);
+// }
 
-#[test]
-fn spec_example_9() {
-    let value = spec_example_json();
-    let q = value.json_path("$..book[?(@.price<10)]").unwrap();
-    assert_eq!(q.len(), 2);
-}
+// #[test]
+// fn spec_example_9() {
+//     let value = spec_example_json();
+//     let q = value.json_path("$..book[?(@.price<10)]").unwrap();
+//     assert_eq!(q.len(), 2);
+// }
 
-#[test]
-fn spec_example_10() {
-    let value = spec_example_json();
-    let q = value.json_path("$..*").unwrap();
-    assert_eq!(q.len(), 27);
-}
+// #[test]
+// fn spec_example_10() {
+//     let value = spec_example_json();
+//     let q = value.json_path("$..*").unwrap();
+//     assert_eq!(q.len(), 27);
+// }
